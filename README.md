b7d1e3d38fd722cc027625e6a68d471c7a9814dd
JparrTech <johnaustinparr1@gmail.com>
2025-04-19 12:21:18 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Meme Generator
+
+I created a meme generator with input fields for top text and bottom text. When the fields are filled out and the button is pressed, the meme generator gets to work and added the text to the image. 
+
+In building this project I learned:
+* How to create controlled components with onChange to update state with every change made to the input field. 
+* Functional Programming: Keeping React Components "pure" by using the React Hook UseEffect.
---
+++ src/components/Main.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+export default function Main() {
+    const [memeInfo, setMemeInfo] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+    function handleChange(event) {
+        const { name, value } = event.currentTarget
+        setMemeInfo(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+        console.log(value)
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                        value = {memeInfo.topText}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value = {memeInfo.bottomText}
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={memeInfo.imageUrl} />
+                <span className="top">{memeInfo.topText}</span>
+                <span className="bottom">{memeInfo.bottomText}</span>
+            </div>
+        </main>
+    )
+}
